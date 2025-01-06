4b3e5b90a85576a644abf4f4dfb2419857a02dd3
Eyoba Smax <eyobsmax@gmail.com>
2025-01-06 21:17:40 +0300

    starting jwt passport stratagy

--- generateKeypair.js
+++ generateKeypair.js
@@ -1,33 +1,28 @@
-/**
- * This module will generate a public and private keypair and save to current directory
- * 
- * Make sure to save the private key elsewhere after generated!
- */
-const crypto = require('crypto');
-const fs = require('fs');
+import crypto from "crypto";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-function genKeyPair() {
-    
-    // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
-    const keyPair = crypto.generateKeyPairSync('rsa', {
-        modulusLength: 4096, // bits - standard for RSA keys
-        publicKeyEncoding: {
-            type: 'pkcs1', // "Public Key Cryptography Standards 1" 
-            format: 'pem' // Most common formatting choice
-        },
-        privateKeyEncoding: {
-            type: 'pkcs1', // "Public Key Cryptography Standards 1"
-            format: 'pem' // Most common formatting choice
-        }
-    });
+async function genKeyPair() {
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: "pkcs1",
+      format: "pem",
+    },
+    privateKeyEncoding: {
+      type: "pkcs1",
+      format: "pem",
+    },
+  });
 
-    // Create the public key file
-    fs.writeFileSync(__dirname + '/id_rsa_pub.pem', keyPair.publicKey); 
-    
-    // Create the private key file
-    fs.writeFileSync(__dirname + '/id_rsa_priv.pem', keyPair.privateKey);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  console.log(__dirname);
 
+  await fs.writeFile(__dirname, keyPair.privateKey);
+
+  await fs.writeFile(__dirname, keyPair.publicKey);
 }
 
-// Generate the keypair
 genKeyPair();
